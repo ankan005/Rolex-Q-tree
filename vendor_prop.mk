3166318b7bcd6ce7db03eb9235f1f14ade53fb9c
nichcream <c.potato23@gmail.com>
2018-12-29 01:51:23 +0000

    rolex: vendor_prop: Set virtual displays to 0
    We don't have any virtual displays, however HWC assumes 1 virtual
    display by default. This was causing laggy display when allocating
    virtual displays, as done by surfaceflinger during screen recording
    for example.
    
    Change-Id: Iab7ddb4182ee1d952f3083942eece033f651af98

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -93,7 +93,8 @@ vendor.display.perf_hint_window=50 \
 video.accelerate.hw=1 \
 debug.hwui.use_buffer_age=false \
 persist.graphics.vulkan.disable=true \
-persist.debug.wfd.enable=1
+persist.debug.wfd.enable=1 \
+debug.sdm.support_writeback=0
 
 # Property to enable display default color mode
 PRODUCT_PROPERTY_OVERRIDES += \
