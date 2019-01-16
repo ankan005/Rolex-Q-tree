32a1fe1faf3491e0d163fbdb332c6c88b3880170
MacTavishOT <ataberkozen123@gmail.com>
2019-01-16 19:30:29 +0530

    rolex: Disable HW overlays
    * While use telegram app quick settings animation work sometimes with framedrops.
    * Also there was framedrops in app switch animation (double tap on recent apps button), but it works better after disabled hw overlay.
    
    Signed-off-by: muralivijay <muralidharan9845@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -87,7 +87,8 @@ vendor.display.perf_hint_window=50 \
 debug.hwui.use_buffer_age=false \
 persist.graphics.vulkan.disable=true \
 persist.debug.wfd.enable=1 \
-debug.sdm.support_writeback=0
+debug.sdm.support_writeback=0 \
+debug.composition.type=gpu
 
 # Property to enable display default color mode
 PRODUCT_PROPERTY_OVERRIDES += \
