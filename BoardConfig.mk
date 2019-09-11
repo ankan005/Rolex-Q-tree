154f867594e5a78a7b7d797a4c837818ab1bb29c
Thagoo <lohitgowda56@gmail.com>
2019-09-11 09:49:32 +0000

    rolex: make permissive and specify kernel version

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -119,7 +119,7 @@ DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78B0000
-#BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_CMDLINE += firmware_class.path=/vendor/firmware_mnt/image
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
@@ -129,6 +129,7 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CONFIG := rolex_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8917
 TARGET_COMPILE_WITH_MSM_KERNEL	:= true
+TARGET_KERNEL_VERSION := 3.18
 
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
