e735969f962ea8b9d27b10eacc615d579f177eaf
Ankan005 <Ankan005@gmail.com>
2019-09-16 10:20:06 +0530

    Q

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,4 +14,7 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_rolex.mk
+    $(LOCAL_DIR)/cos__rolex.mk
+
+COMMON_LUNCH_CHOICES := \
+    cos_rolex-userdebug
--- cos_rolex.mk
+++ cos_rolex.mk
@@ -17,9 +17,9 @@
 $(call inherit-product, device/xiaomi/rolex/full_rolex.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/cos/config/common.mk)
 
-PRODUCT_NAME := lineage_rolex
+PRODUCT_NAME := cos_rolex
 BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
