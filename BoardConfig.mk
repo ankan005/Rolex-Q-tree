54a2f7a7470ba5c45ba147453a7525ba5ba13884
Ankan005 <Ankan005@gmail.com>
2019-09-16 17:21:11 +0530

    neverallow

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,6 +16,12 @@
 
 DEVICE_PATH := device/xiaomi/rolex
 
+
+
+ifneq ($(TARGET_BUILD_VARIANT),user)
+SELINUX_IGNORE_NEVERALLOWS := true
+endif
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
